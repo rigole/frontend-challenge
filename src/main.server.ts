983665aa23b48babca0e2ba5104d85b979683fca
Placide Rigole <foplacide@gmail.com>
2025-10-13 18:07:33 +0100

    reparing starting file

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {bootstrapApplication, BootstrapContext} from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { provideServerRendering } from '@angular/platform-server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
 
-export default bootstrap;
+
+export default function bootstrap(context: BootstrapContext) {
+  return bootstrapApplication(AppComponent, {
+    ...config,
+    providers: [
+      provideServerRendering(),
+      ...(config.providers || []),
+    ],
+  }, context);
+}
